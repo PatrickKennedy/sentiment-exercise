6fceaba795e06b302e3f58b87e75ad190b482869
Patrick Kennedy <pakattack161@gmail.com>
2016-01-25 21:05:20 -0700

    add format and clean utility methods to the Analyzer

--- src/sentiment.js
+++ src/sentiment.js
@@ -3,6 +3,20 @@ var Analyzer = function(dictionary){
   self.dictionary = dictionary;
 };
 
+Analyzer.prototype.format = function(words, options) {
+  options = options || {};
+  return words.split(options.delimiter || ' ');
+};
+
+Analyzer.prototype.clean = function(words, options) {
+  options = options || {};
+  return words.map(function(word){
+    word = word.toLowerCase();
+    word = word.replace(options.regex || /\W+/, '');
+    return word;
+  });
+};
+
 Analyzer.prototype.process = function(words) {
   var self = this;
   // quick and dirty way of turning a string into an array but keeping arrays
--- src/sentiment.spec.js
+++ src/sentiment.spec.js
@@ -13,36 +13,67 @@ describe('socialytics', function() {
     });
 
     describe('analyzer', function() {
-      it('should return undefined for undocumented words', function(){
-        expect(analyzer.process(['is'])).to.be.undefined;
-        expect(analyzer.process(["isn't"])).to.be.undefined;
-        expect(analyzer.process(['the'])).to.be.undefined;
-        expect(analyzer.process(['math'])).to.be.undefined;
+      describe('.format', function() {
+        it('should return an array of words split by space', function(){
+          expect(analyzer.format('bacon and eggs'))
+            .to.have.members(['bacon', 'and', 'eggs']);
+        });
+
+        it('should return an array of words split by the passed delimiter', function(){
+          expect(analyzer.format('bacon, and, eggs', {delimiter: ', '}))
+            .to.have.members(['bacon', 'and', 'eggs']);
+        });
       });
 
-      it('should return 0 for neutral words', function(){
-        expect(analyzer.process(['yeah'])).to.equal(0);
+      describe('.clean', function() {
+        var split_text = [];
+        beforeEach(function(){
+          split_text = analyzer.format("Bacon is #great ain't it?");
+        });
+
+        it('should return the array lowered and cleaned of non-word characters', function(){
+          expect(analyzer.clean(split_text))
+            .to.have.members(['bacon', 'is', 'great', 'aint', 'it']);
+        });
+
+        it('should return the array lowered and cleaned of custom characters', function(){
+          expect(analyzer.clean(split_text, {regex: /[in]+/}))
+            .to.have.members(['baco', 's', '#great', "a't", 't?']);
+        });
       });
 
-      it('should return 1 for positive words', function(){
-        expect(analyzer.process(['encouragingly'])).to.equal(1);
-        expect(analyzer.process(['dance'])).to.equal(1);
-      });
+      describe('.process', function() {
+        it('should return undefined for undocumented words', function(){
+          expect(analyzer.process(['is'])).to.be.undefined;
+          expect(analyzer.process(["isn't"])).to.be.undefined;
+          expect(analyzer.process(['the'])).to.be.undefined;
+          expect(analyzer.process(['math'])).to.be.undefined;
+        });
 
-      it('should return -1 for negative words', function(){
-        expect(analyzer.process(['dismal'])).to.equal(-1);
-        expect(analyzer.process(['unfortunately'])).to.equal(-1);
-        expect(analyzer.process(['futile'])).to.equal(-1);
-      });
+        it('should return 0 for neutral words', function(){
+          expect(analyzer.process(['yeah'])).to.equal(0);
+        });
 
-      it('should return net negative for sentences with more negative than positive words', function(){
-        expect(analyzer.process(['unfortunately', 'dance', 'futile'])).to.equal(-1);
-        expect(analyzer.process(['unfortunately', 'the', 'math', 'dance', 'is', 'futile'])).to.equal(-1);
-      });
+        it('should return 1 for positive words', function(){
+          expect(analyzer.process(['encouragingly'])).to.equal(1);
+          expect(analyzer.process(['dance'])).to.equal(1);
+        });
 
-      it('should return net negative for sentences with more negative than positive words', function(){
-        expect(analyzer.process(['encouragingly', 'dance', 'futile'])).to.equal(1);
-        expect(analyzer.process(['encouragingly', 'the', 'math', 'dance', "isn't", 'futile'])).to.equal(1);
+        it('should return -1 for negative words', function(){
+          expect(analyzer.process(['dismal'])).to.equal(-1);
+          expect(analyzer.process(['unfortunately'])).to.equal(-1);
+          expect(analyzer.process(['futile'])).to.equal(-1);
+        });
+
+        it('should return net negative for sentences with more negative than positive words', function(){
+          expect(analyzer.process(['unfortunately', 'dance', 'futile'])).to.equal(-1);
+          expect(analyzer.process(['unfortunately', 'the', 'math', 'dance', 'is', 'futile'])).to.equal(-1);
+        });
+
+        it('should return net negative for sentences with more negative than positive words', function(){
+          expect(analyzer.process(['encouragingly', 'dance', 'futile'])).to.equal(1);
+          expect(analyzer.process(['encouragingly', 'the', 'math', 'dance', "isn't", 'futile'])).to.equal(1);
+        });
       });
     });
   });
